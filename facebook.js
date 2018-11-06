e14c382acf6794a0e6c3e4ee484393e6c5059200
kazenkun <36505232+kazenkun@users.noreply.github.com>
2018-11-06 20:20:42 +0700

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,6 @@
+var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = &quot;//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.4&quot;;
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, &#39;script&#39;, &#39;facebook-jssdk&#39;));
